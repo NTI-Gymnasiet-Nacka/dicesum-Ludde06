c41b5bfd0cec2c41005f77ee5f99dcc0a81ec88e
Ludwig M <ludwig@metelius.nu>
2024-08-22 10:27:55 +0200

    klar

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 # Dice sum probability calculator
-# Författare: 
-# Datum:
+# Författare: Ludwig Metelius
+# Datum: 2024-08-22
 
 def main():
-    user_input = input().split(" ")
-    print()
+    N, M = input("").split()
+
+    lista = []
+    dictionary = {}
+
+    for n in range (1, int(N)+1):
+        for m in range (1, int(M)+1):
+            lista.append(n+m)
+
+    for u in range(0, 40):
+        dictionary[u] = int(lista.count(u))
+
+    maxvalue = max(dictionary.values())
+
+    for i, o in dictionary.items():
+        if o == maxvalue:
+            print(i)
+        
 
 if __name__ == "__main__":
     main()
